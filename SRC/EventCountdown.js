a1333bedcbccd69b275bfc0fe5d572666b989eaa
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-04-06 22:00:10 +0200

    Added EventCountdown.js

---
+++ SRC/EventCountdown.js
@@ -0,0 +1,26 @@
+const countDownDate = new Date("May 3, 2022 00:00:00").getTime();
+
+const x = setInterval(function() {
+
+
+  const now = new Date().getTime();
+    
+
+  let distance = countDownDate - now;
+    
+  
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "VERLOPEN";
+  }
+}, 1000);
